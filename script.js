2a3305e5dcdb6753738df0abeaacdf0585659d38
emitho <emil.thorsplass@gmail.com>
2023-05-08 14:49:54 -0300

    Add functionality, styles and animations to business card section

--- script.js
+++ script.js
@@ -41,3 +41,37 @@ function moveToNext() {
 // Add event listeners to the carousel controls
 prevButton.addEventListener('click', moveToPrev);
 nextButton.addEventListener('click', moveToNext);
+
+
+// Function for the fade-up effect on contact card
+
+document.addEventListener('DOMContentLoaded', function () {
+  AOS.init();
+});
+
+// Latest Tweet loading place holder
+window.twttr = (function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+twttr.ready(function (twttr) {
+  twttr.events.bind('loaded', function (event) {
+    const tweetPlaceholder = document.querySelector('.tweet-placeholder');
+    tweetPlaceholder.style.display = 'none';
+  });
+});
+
+
