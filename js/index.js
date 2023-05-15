f3ad6d9cbf85e0b9dfbba7ce4afeacd73264f6ff
emitho <emil.thorsplass@gmail.com>
2023-05-15 14:56:49 -0300

    Fix issue with carousel arrows, style hamburger & mobile responsiveness

--- js/index.js
+++ js/index.js
@@ -1,58 +1,60 @@
-// Carousel functionality
-const carouselTrack = document.querySelector('.carousel-track');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const prevButton = document.querySelector('.carousel-control.prev');
-const nextButton = document.querySelector('.carousel-control.next');
-const carouselContainer = document.querySelector('.carousel-container');
+document.addEventListener('DOMContentLoaded', function() {
+  // Carousel functionality
+  const carouselTrack = document.querySelector('.carousel-track');
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  const prevButton = document.querySelector('.carousel-control.prev');
+  const nextButton = document.querySelector('.carousel-control.next');
+  const carouselContainer = document.querySelector('.carousel-container');
 
-if (carouselItems.length > 0) {
-  let currentPosition = 0;
-  const visibleItems = 4;
-  const cardWidth = carouselItems[0].clientWidth + 2 * parseInt(getComputedStyle(carouselItems[0]).marginRight);
+  if (carouselItems.length > 0) {
+    let currentPosition = 0;
+    const visibleItems = 4;
+    const cardWidth = carouselItems[0].clientWidth + 2 * parseInt(getComputedStyle(carouselItems[0]).marginRight);
 
-  function moveToPrev() {
-    currentPosition += cardWidth * visibleItems;
-    if (currentPosition > 0) {
-      currentPosition = 0;
+    function moveToPrev() {
+      currentPosition += cardWidth * visibleItems;
+      if (currentPosition > 0) {
+        currentPosition = 0;
+      }
+      carouselTrack.style.transform = `translateX(${currentPosition}px)`;
+      checkArrows();
     }
-    carouselTrack.style.transform = `translateX(${currentPosition}px)`;
-    checkArrows();
+
+    function moveToNext() {
+      const trackWidth = carouselTrack.scrollWidth;
+      const containerWidth = carouselContainer.clientWidth;
+      currentPosition -= cardWidth * visibleItems;
+      if (currentPosition < -(trackWidth - containerWidth)) {
+        currentPosition = -(trackWidth - containerWidth);
+      }
+      carouselTrack.style.transform = `translateX(${currentPosition}px)`;
+      checkArrows();
+    }
+
+    function checkArrows() {
+      if (currentPosition === 0) {
+        prevButton.disabled = true;
+      } else {
+        prevButton.disabled = false;
+      }
+
+      const trackWidth = carouselTrack.scrollWidth;
+      const containerWidth = carouselContainer.clientWidth;
+
+      if (currentPosition <= -(trackWidth - containerWidth - cardWidth * (visibleItems - 1))) {
+        nextButton.disabled = true;
+      } else {
+        nextButton.disabled = false;
+      }
+    }
+
+    // Add event listeners to the carousel controls
+    prevButton.addEventListener('click', moveToPrev);
+    nextButton.addEventListener('click', moveToNext);
+
+    checkArrows(); // Call this function once on load to disable the left arrow initially
   }
-
-  function moveToNext() {
-    const trackWidth = carouselTrack.scrollWidth;
-    const containerWidth = carouselContainer.clientWidth;
-    currentPosition -= cardWidth * visibleItems;
-    if (currentPosition < -(trackWidth - containerWidth)) {
-      currentPosition = -(trackWidth - containerWidth);
-    }
-    carouselTrack.style.transform = `translateX(${currentPosition}px)`;
-    checkArrows();
-  }
-
-  function checkArrows() {
-    if (currentPosition === 0) {
-      prevButton.disabled = true;
-    } else {
-      prevButton.disabled = false;
-    }
-  
-    const trackWidth = carouselTrack.scrollWidth;
-    const containerWidth = carouselContainer.clientWidth;
-  
-    if (currentPosition <= -(trackWidth - containerWidth - cardWidth * (visibleItems - 1))) {
-      nextButton.disabled = true;
-    } else {
-      nextButton.disabled = false;
-    }
-  }
-
-  // Add event listeners to the carousel controls
-  prevButton.addEventListener('click', moveToPrev);
-  nextButton.addEventListener('click', moveToNext);
-
-  checkArrows(); // Call this function once on load to disable the left arrow initially
-}
+});
 
 // Function for the fade-up effect on contact card
 document.addEventListener("DOMContentLoaded", function () {
