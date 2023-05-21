3755ca73893d37963a4e49517f09f41b77122cb1
emitho <emil.thorsplass@gmail.com>
2023-05-21 17:30:51 -0300

    Update fetch URLs in JS files to fix wordpress content netlify issue

--- js/blog-specific.js
+++ js/blog-specific.js
@@ -1,13 +1,32 @@
-// Query string 
-
+// Query string function
 function getQueryStringValue(key) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(key);
 }
 
+// Get the postId from the URL query parameter
+const postId = getQueryStringValue('postId');
+
+// Fetch and display the specific blog post content
+if (postId) {
+  fetch(`https://emilandret.sg-host.com/wp-json/wp/v2/posts/${postId}`)
+    .then(response => response.json())
+    .then(data => {
+      const blogPostContent = document.getElementById('blog-specific-content');
+
+      // Update the blog-specific content section
+      blogPostContent.innerHTML = `
+        <h2>${data.title.rendered}</h2>
+        <div>${data.content.rendered}</div>
+      `;
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+}
 
 // Modal
-
 document.querySelectorAll('.article-image').forEach((imgContainer) => {
   imgContainer.addEventListener('click', (event) => {
     const imageUrl = event.target.src;
@@ -23,25 +42,3 @@ document.getElementById('image-modal').addEventListener('click', () => {
   const modal = document.getElementById('image-modal');
   modal.style.display = 'none';
 });
-
-// Get the postId from the URL query parameter
-const urlParams = new URLSearchParams(window.location.search);
-const postId = urlParams.get('postId');
-
-// Fetch the specific blog post from WordPress
-fetch(`http://emilandret.sg-host.com/wp-json/wp/v2/posts/${postId}`)
-  .then(response => response.json())
-  .then(data => {
-    // Handle the retrieved blog post data
-    const blogPostContent = document.getElementById('blog-specific-content');
-
-    // Update the blog-specific content section
-    blogPostContent.innerHTML = `
-      <h2>${data.title.rendered}</h2>
-      <div>${data.content.rendered}</div>
-    `;
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-  });
